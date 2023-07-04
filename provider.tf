dc23f7445fe0318d9ba65fd227c5060322a84b73
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-07-04 11:56:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                   = "us-east-1"
+  region                   = "ap-south-1"
   access_key               = "AKIA5BMGLEQ5VPTWYFT2"
   secret_key               = "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"
 }
